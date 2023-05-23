d29fca607430b93af2807a783b60641891597cab
Paulo Becker <beckerpaulo81@gmail.com>
2023-05-23 13:56:00 -0300

    Aula 21 - API Spring: criando cursos + adicionando HTTP POST

--- src/main/java/com/becker/controller/CourseController.java
+++ src/main/java/com/becker/controller/CourseController.java
@@ -3,10 +3,9 @@ package com.becker.controller;
 import com.becker.model.Course;
 import com.becker.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,12 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+//        System.out.println(course.getName());
+        return courseRepository.save(course);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
 
 }
