d1046f6af8ab05ce8bfb28f72d04a343604a6d70
Paulo Becker <beckerpaulo81@gmail.com>
2023-06-01 10:55:12 -0300

    Aula | 30:Atualiozar curso API Spring HTTP PUT - criado o método para atualizar um registro na base na API do Spring com HTTP PUT.

--- src/main/java/com/becker/controller/CourseController.java
+++ src/main/java/com/becker/controller/CourseController.java
@@ -19,24 +19,36 @@ public class CourseController {
 
 
     @GetMapping
-    public @ResponseBody List<Course> list(){
+    public @ResponseBody List<Course> list() {
 
         return courseRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id){
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
         return courseRepository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course){
+    public Course create(@RequestBody Course course) {
 //        System.out.println(course.getName());
         return courseRepository.save(course);
 //        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
