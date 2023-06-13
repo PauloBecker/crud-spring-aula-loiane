9957397b0bcdb5434c21f9246a63c13083409549
Paulo Becker <beckerpaulo81@gmail.com>
2023-06-13 19:18:55 -0300

    Aula 36 - Remoção lógica - fazendo o soft delete, ou remoção lógica usando Spring + Hibernate. Explico duas formas diferentes de se chegar no mesmo resultado.

--- src/main/java/com/becker/model/Course.java
+++ src/main/java/com/becker/model/Course.java
@@ -6,10 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -29,5 +33,10 @@ public class Course {
     @Column(length = 10, nullable = false)
     private String category;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 
 }
